c621fcc05cbaef56264a78ea2861591ec52cf615
AberDerBart <nonatz@web.de>
2017-12-05 17:24:25 +0100

    added command line arguments

--- mpw.py
+++ mpw.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
-
 import mpd
 import json
+import argparse
+import sys
 
 c=mpd.MPDClient()
 
-c.connect("faultier",6600)
+def isAvailable():
+	return True
 
-c.subscribe("scheduled")
-
-alarms=None
-
-def listAlarms():
+def getTasks():
 	alarms=None
 	c.sendmessage("scheduler","list_json")
 	c.idle("message")
@@ -20,5 +18,45 @@ def listAlarms():
 			alarms=json.loads(m["message"])
 	return alarms
 
-def addAlarm(h,m):
-	c.sendmessage("scheduler","alarm "+str(h)+":"+str(m))
+def addAlarm(time):
+	c.sendmessage("scheduler","alarm "+time)
+
+def addSleep(time):
+	c.sendmessage("scheduler","alarm "+time)
+
+def cancel(index):
+	c.sendmessage("scheduler","cancel "+index)
+
+def main():
+	parser=argparse.ArgumentParser()
+	subParsers=parser.add_subparsers(dest="command")
+	subParsers.required=False
+
+	alarmParser=subParsers.add_parser("alarm",help="sets an alarm")
+	alarmParser.add_argument("time",help="time specified in the format +MIN or HH:MM")
+
+	sleepParser=subParsers.add_parser("sleep",help="sets a sleep timer")
+	sleepParser.add_argument("time",help="time specified in the format +MIN or HH:MM")
+
+	listParser=subParsers.add_parser("list",help="lists the scheduled tasks")
+
+	cancelParser=subParsers.add_parser("cancel",help="cancels the specified tasks")
+	cancelParser.add_argument("index", help="index of the task to be canceled")
+
+	args=parser.parse_args()
+
+	c.connect("localhost",6600)
+
+	c.subscribe("scheduled")
+
+	if(not isAvailable()):
+		print("MpdScheduler not available.",file=sys.stdout)
+		exit(-1)
+
+	if(not args.command or args.command=="list"):
+		print("list")
+	else:
+		pass
+
+if __name__ == '__main__':
+	main()
